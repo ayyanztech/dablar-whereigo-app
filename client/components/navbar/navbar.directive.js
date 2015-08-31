13cbcbd77b5f3892948f3a3bb32393f53078c6dc
dablar <daniel.blara@gmail.com>
2015-08-31 13:03:03 +0200

    initial commit

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('dablarWhereigoAppApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
